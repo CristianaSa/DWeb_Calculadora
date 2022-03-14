94bfa6ed2d3e8fe7f21f1468bba7619f9b899d3d
Cristiana Santana <cristianasantanaa1992@gmail.com>
2022-03-14 13:31:47 +0000

    Design Calculadora

--- Calculadora/Calculadora/wwwroot/js/site.js
+++ Calculadora/Calculadora/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+ 
+   function darkMode() {
+   var element = document.body;
+    element.classList.toggle("dark-mode");
+}
