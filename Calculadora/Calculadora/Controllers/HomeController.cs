7d3b54be383a19e9a76bc1d78ec88855336ee625
CristianaSa <cristianasantanaa1992@gmail.com>
2022-03-14 22:21:24 +0000

    Calculadora - ViewBag Passar dados para o visor -Numeros (Aula)

--- Calculadora/Calculadora/Controllers/HomeController.cs
+++ Calculadora/Calculadora/Controllers/HomeController.cs
@@ -12,9 +12,43 @@ namespace Calculadora.Controllers
         {
             _logger = logger;
         }
-
+        [HttpGet] // Esta anotação é facultativa
         public IActionResult Index()
         {
+            //Inicializar a calculadora
+            ViewBag.Visor = "0";
+            return View();
+        }
+
+        [HttpPost] //Esta anotação já é obrigatória, para o método 'escutar'
+        public IActionResult Index(string botao, string visor)
+        {
+           switch (botao)
+            {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    //Atribuir ao visor o algarismo selecionado
+                    if (visor != "0") visor = visor + botao;
+                    else visor = botao;
+                    break;
+                case "C":
+                    //limpar o ecrã
+                    visor = "0";
+                    break;
+                case "+":
+                    break;
+            }
+
+            //Preparar os dados a serem enviados para a View
+            ViewBag.Visor = visor;
             return View();
         }
 
