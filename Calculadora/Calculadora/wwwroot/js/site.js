994d7e91c2c4cf0dbc2ded95be3a5345b0d90455
Cristiana Santana <cristianasantanaa1992@gmail.com>
2022-03-15 12:07:54 +0000

    Calculadora Layout

--- Calculadora/Calculadora/wwwroot/js/site.js
+++ Calculadora/Calculadora/wwwroot/js/site.js
@@ -10,8 +10,24 @@
        element.classList.toggle("dark-mode");
        x.classList.toggle("fa-sun");
 
-
-
-     
         
+}
+
+// This function clear all the values
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+
+// This function display values
+function display(value) {
+    
+    document.getElementById("result").value += value;
+}
+ 
+
+// This function evaluates the expression and return result
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
 }
